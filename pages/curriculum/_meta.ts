1745b88aeb0198940e2e6827f3ae85bcb25dc934
Jimmy Guzman <Jimmy.GuzmanMoreno@target.com>
2024-04-23 12:53:47 -0500

    refactor: ♻️ normalize weeks by adding a configuration (#46)

--- pages/curriculum/_meta.ts
+++ pages/curriculum/_meta.ts
@@ -1,3 +1,13 @@
+const composeWeeksConfig = (totalWeeks = 9) => {
+  return Object.fromEntries(
+    Array.from({ length: totalWeeks }, (_value, index) => {
+      return [`week_${index}`, `Week ${index}`];
+    }),
+  );
+};
+
 export default {
   index: "Overview",
+  goals: "Goals",
+  ...composeWeeksConfig(),
 };
