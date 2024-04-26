3edc70e20dfbfac1fe156e586272a4b695b1a5f9
Jimmy Guzman <Jimmy.GuzmanMoreno@target.com>
2024-04-26 16:09:41 -0500

    fix: 🐛 hide pages that are meant to serve as a resource

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,7 +1,12 @@
 export default {
   index: "Home",
   curriculum: "Curriculum",
-  slides: "Slides",
   project: "Project",
   resources: "Additional Resources",
+  slides: {
+    display: "hidden",
+  },
+  notes: {
+    display: "hidden",
+  },
 };
