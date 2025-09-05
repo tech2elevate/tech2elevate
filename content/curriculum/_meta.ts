548a710ca4892930f641ebdf5d102447b994e23a
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-09-04 22:03:27 -0500

    refactor: ♻️ avoid week 0 for clarity (#25)

--- content/curriculum/_meta.ts
+++ content/curriculum/_meta.ts
@@ -1,17 +1,4 @@
-const composeWeeksConfig = (totalWeeks = 8) => {
-  return Object.fromEntries(
-    Array.from({ length: totalWeeks }, (_value, index) => {
-      if (index === 0) {
-        return [`week_${index}`, "Kickoff"];
-      } else {
-        return [`week_${index}`, `Week ${index}`];
-      }
-    }),
-  );
-};
-
 export default {
   index: "Overview",
   goals: "Goals",
-  ...composeWeeksConfig(),
 };
